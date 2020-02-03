b69283240055b45db6d3cdfb19ae16ec2164e14b
Catink123 <qwerty.qaz120@gmail.com>
2020-02-03 20:47:18 +0300

    Updates

---
+++ precache-manifest.506e69c5f34d917ac5945b8b747fac31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c937eb8e45e3c4257dd73fb045364db8",
+    "url": "/mob/index.html"
+  },
+  {
+    "revision": "15cd51112e6842c672da",
+    "url": "/mob/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f9c45d5d85ff77bb7adf",
+    "url": "/mob/static/js/2.d7e8314f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/mob/static/js/2.d7e8314f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15cd51112e6842c672da",
+    "url": "/mob/static/js/main.2bcf8a51.chunk.js"
+  },
+  {
+    "revision": "eb051f09f34fc88e3ec5",
+    "url": "/mob/static/js/runtime-main.2c38b0d4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mob/static/media/logo.5d5d9eef.svg"
+  }
+]);
